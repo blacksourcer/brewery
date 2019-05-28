3c197d591998e083733d133f602258544ae78d1b
blacksourcer <blacksourcer@wisebits.com>
2019-05-28 13:10:40 +0300

    Router added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from '../../pages/home';
+import SignIn from '../../pages/sign-in';
+
 import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li><Link to="/sign-in">Sign In</Link></li>
+    </ul>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/sign-in" component={SignIn} />
+  </Router>
+);
 
-export default App;
+export default App
